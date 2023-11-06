96fcb15814680167b9a485626c8cf7e6899a6bad
Kazen Gallman <kazengallman@kazens-mbp.rlab.cs.brown.edu>
2023-11-06 14:52:39 -0500

    added colors and basic wifi

---
+++ test.py
@@ -0,0 +1,20 @@
+from serial import Serial
+import requests
+import json 
+import time
+
+ser = Serial(port='/dev/cu.usbmodem14201', baudrate=9600)
+
+text = "on"
+#print(requests.get('https://kazar4.com:5672/lights/', verify=False).text)
+
+while True:
+    time.sleep(1)
+    toSend = requests.get('https://kazar4.com:5672/lights/', verify=False).text
+    print("Trying To Send: " + str(toSend))
+    ser.write(toSend.encode())
+    #ser.write(input("Input 1 to turn on, or 0 to turn off the built-in LED: ").encode())
+        #input("Input 1 to turn on, or 0 to turn off the built-in LED: ").encode())
+    #value= ser.readline()
+    #valueInString=str(value,'UTF-8')
+    #print(valueInString) 
