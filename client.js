240f8c34e33714ea84da5ef528f8b92b0dafdc94
Kazen Gallman <kazengallman@kazens-mbp.devices.brown.edu>
2023-11-06 02:06:17 -0500

    changing fetch req

--- client.js
+++ client.js
@@ -1,7 +1,7 @@
 
 
 function sendClick(color) {
-    fetch("http://" + document.location.hostname + ":5672/lights/" + color)
+    fetch("https://" + document.location.hostname + ":5672/lights/" + color)
   .then((response) => response.json())
   .then((json) => console.log(json));
 }
