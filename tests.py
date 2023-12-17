972edb5b81f864ecd852beb9af8d700a5186c01e
Kazen Gallman <kazar444@icloud.com>
2023-12-17 18:38:30 -0500

    finished tests

--- tests.py
+++ tests.py
@@ -5,8 +5,10 @@ import time
 # https://github.com/websocket-client/websocket-client
 # pip3 install websocket-client
 
-class TestStringMethods(unittest.TestCase):
+class TestWebsocketMethods(unittest.TestCase):
 
+    # tests basic integration between mocked webapp and mocked ESP
+    # both can send commands to each other
     def testPingPong(self):
         ws = create_connection("wss://www.kazar4.com:9001", sslopt = {"cert_reqs": ssl.CERT_NONE})
         ws2 = create_connection("wss://www.kazar4.com:9001", sslopt = {"cert_reqs": ssl.CERT_NONE})
@@ -19,7 +21,7 @@ class TestStringMethods(unittest.TestCase):
         time.sleep(1)
 
         ws.send("Ping")
-        print("Sent Ping")
+        #print("Sent Ping")
         result = ws2.recv()
 
         ws.close()
@@ -27,6 +29,7 @@ class TestStringMethods(unittest.TestCase):
 
         self.assertEqual(result, 'Ping')
     
+    # tests that a mock ESP can be created
     def testESP(self):
         ws = create_connection("wss://www.kazar4.com:9001", sslopt = {"cert_reqs": ssl.CERT_NONE})
 
@@ -38,6 +41,7 @@ class TestStringMethods(unittest.TestCase):
 
         self.assertEqual(result, 'ESP Connected')
     
+    # tests that a mock player can be created
     def testPlayer1(self):
         ws = create_connection("wss://www.kazar4.com:9001", sslopt = {"cert_reqs": ssl.CERT_NONE})
 
@@ -48,31 +52,44 @@ class TestStringMethods(unittest.TestCase):
         ws.close()
 
         self.assertEqual(result, 'player1 Connected')
+    
+    # tests that an invalid game command is thrown out/ignored
+    def invalidCommand(self):
+        ws = create_connection("wss://www.kazar4.com:9001", sslopt = {"cert_reqs": ssl.CERT_NONE})
 
-    # def setESP():
-    #     ws = create_connection("wss://www.kazar4.com:9001", sslopt = {"cert_reqs": ssl.CERT_NONE})
-    #     print(ws.recv())
-    #     print("Sending 'Hello, World'...")
-    #     ws.send("Hello, World")
-    #     print("Sent")
-    #     print("Receiving...")
-    #     result =  ws.recv()
-    #     print("Received '%s'" % result)
-    #     ws.close()
+        ws.send("player1")
+        ws.recv()
 
-    def test_upper(self):
-        self.assertEqual('foo'.upper(), 'FOO')
+        time.sleep(1)
 
-    def test_isupper(self):
-        self.assertTrue('FOO'.isupper())
-        self.assertFalse('Foo'.isupper())
+        ws.send("A VERY INVALID COMMAND")
 
-    def test_split(self):
-        s = 'hello world'
-        self.assertEqual(s.split(), ['hello', 'world'])
-        # check that s.split fails when the separator is not a string
-        with self.assertRaises(TypeError):
-            s.split(2)
+        result = ws.recv()
+
+        ws.close()
+
+        self.assertEqual(result, 'invalid command')
+    
+    # tests that a valid game command is sent from player to ESP
+    def testGameMove(self):
+        ws = create_connection("wss://www.kazar4.com:9001", sslopt = {"cert_reqs": ssl.CERT_NONE})
+        ws2 = create_connection("wss://www.kazar4.com:9001", sslopt = {"cert_reqs": ssl.CERT_NONE})
+
+        ws.send("player1")
+        ws2.send("ESP")
+        ws.recv()
+        ws2.recv()
+
+        time.sleep(1)
+
+        ws.send("U")
+        #print("Sent Up")
+        result = ws2.recv()
+
+        ws.close()
+        ws2.close()
+
+        self.assertEqual(result, 'U')
 
 if __name__ == '__main__':
     unittest.main()
